9a1323b4aa5dfafebde081bdbe6774fbc2c564e2
Rajiv Kaushik <zayeemzaki45@gmail.com>
2024-09-26 13:04:06 -0400

    updated url

--- PushNewException.py
+++ PushNewException.py
@@ -2,7 +2,7 @@ import requests
 
 API_KEY = "api_key"
 rule_rrn = "rule_rrn"
-url = f"https://us.api.insight.rapid7.com/idr/v1/rules/{rule_rrn}/rule-exceptions/create"
+url = f"https://us3.api.insight.rapid7.com/idr/v1/rules/{rule_rrn}/rule-exceptions/create"
 
 headers = {
     "X-Api-Key": API_KEY,
